d06068fdd884f7bc9403ada4549c526bf478a7b9
GBansal <customer@DESKTOP-HBA9GC2>
2023-10-05 14:17:01 +0100

    First movie crud asp.net core app with EF and db support

---
+++ MyCoreWebApp/Data/MyCoreWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyCoreWebApp.Models;
+
+namespace MyCoreWebApp.Data
+{
+    public class MyCoreWebAppContext : DbContext
+    {
+        public MyCoreWebAppContext (DbContextOptions<MyCoreWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyCoreWebApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MyCoreWebApp/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyCoreWebApp.Data;
+using MyCoreWebApp.Models;
+
+namespace MyCoreWebApp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MyCoreWebApp.Data.MyCoreWebAppContext _context;
+
+        public CreateModel(MyCoreWebApp.Data.MyCoreWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
